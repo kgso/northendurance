2ca53346258f639dc6165b34ee98b0fc7c60cc46
Sarah Morash <sarah.morash@dal.ca>
2021-03-17 21:44:22 -0300

    feat: add basic tolgging of mobile nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,8 +46,7 @@ const Header = () => {
           </div>
         </div>
         <div
-          className={`${!mobileOpen && "hidden"
-            } md:block md:ml-10 md:pr-4 md:space-x-8`}
+          className="hidden md:block md:ml-10 md:pr-4 md:space-x-8"
         >
           <Link
             to="/about"
@@ -86,94 +85,97 @@ const Header = () => {
           </a>
         </div>
       </nav>
-      <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-          <div className="pt-5 pb-6 px-5">
-            <div className="flex items-center justify-between">
-              <div>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                  alt="Workflow"
-                />
-              </div>
-              <div className="-mr-2">
-                <button
-                  type="button"
-                  className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                >
-                  <span className="sr-only">Close menu</span>
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
+      {mobileOpen && (
+        <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+            <div className="pt-5 pb-6 px-5">
+              <div className="flex items-center justify-between">
+                <div>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                    alt="Workflow"
+                  />
+                </div>
+                <div className="-mr-2">
+                  <button
+                    onClick={() => setMobileOpen(!mobileOpen)}
+                    type="button"
+                    className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
+                    <span className="sr-only">Close menu</span>
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="mt-6">
-              <nav className="grid gap-y-8">
-                <Link
-                  to="/about"
-                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                >
-                  <span className="ml-3 text-base font-medium text-gray-900">
-                    About
+              <div className="mt-6">
+                <nav className="grid gap-y-8">
+                  <Link
+                    to="/about"
+                    className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    <span className="ml-3 text-base font-medium text-gray-900">
+                      About
                   </span>
-                </Link>
+                  </Link>
 
-                <Link
-                  to="/programs"
-                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                >
-                  <span className="ml-3 text-base font-medium text-gray-900">
-                    Programs
+                  <Link
+                    to="/programs"
+                    className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    <span className="ml-3 text-base font-medium text-gray-900">
+                      Programs
                   </span>
-                </Link>
+                  </Link>
 
-                <Link
-                  to="/schedule"
-                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                >
-                  <span className="ml-3 text-base font-medium text-gray-900">
-                    Schedule
+                  <Link
+                    to="/schedule"
+                    className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    <span className="ml-3 text-base font-medium text-gray-900">
+                      Schedule
                   </span>
-                </Link>
+                  </Link>
 
-                <Link
-                  to="/faq"
-                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                >
-                  <span className="ml-3 text-base font-medium text-gray-900">
-                    FAQ
+                  <Link
+                    to="/faq"
+                    className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    <span className="ml-3 text-base font-medium text-gray-900">
+                      FAQ
                   </span>
-                </Link>
+                  </Link>
 
-                <a
-                  href="https://crossfitkineticsca.sites.zenplanner.com/login.cfm"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                >
-                  <span className="ml-3 text-base font-medium text-gray-900">
-                    ZenPlanner
+                  <a
+                    href="https://crossfitkineticsca.sites.zenplanner.com/login.cfm"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    <span className="ml-3 text-base font-medium text-gray-900">
+                      ZenPlanner
                   </span>
-                </a>
-              </nav>
+                  </a>
+                </nav>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
