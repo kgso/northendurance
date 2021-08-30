6f1170d9a5df4976f21103758044cd97a30858f8
Sarah Morash <sarahjmorash@gmail.com>
2021-08-30 16:15:36 -0300

    chore: update GA tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-202VBPM8YWa",
+        trackingId: "UA-141718999-3",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Delays sending pageview hits on route update (in milliseconds)
